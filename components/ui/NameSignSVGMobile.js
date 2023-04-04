bc74904523f812f5148f4ede470df3b06f875752
Gregori <greg.gego@gmail.com>
2023-04-04 21:09:01 +0200

    logo animation

--- components/ui/NameSignSVGMobile.js
+++ components/ui/NameSignSVGMobile.js
@@ -1,6 +1,6 @@
 import Spline from '@splinetool/react-spline';
 
-export default function NameSignSVGMobile() {
+export default function NameSignSvgMobile() {
   return (
     <Spline scene="https://prod.spline.design/1bjwvg19QY6kbzky/scene.splinecode" />
   );
